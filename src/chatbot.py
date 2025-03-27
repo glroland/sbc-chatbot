8373f1db78b47c444776222c2036b784682d95bd
Lee Roland <glroland@hotmail.com>
2025-03-27 14:56:35 -0400

    working chatbot w/o rag

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,10 +1,28 @@
 import streamlit as st
 import random
 import time
+from openai import OpenAI
 
-st.write("Streamlit loves LLMs! 🤖 [Build your own chat app](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps) in minutes, then make it powerful by adding images, dataframes, or even input widgets to the chat.")
+OPENAI_URL = "http://envision:11434/v1"
+OPENAI_KEY = "not_used"
+OPENAI_TIMEOUT = 1000
+OPENAI_MODEL = "llama3.2"
+OPENAI_TEMPERATURE = 0.8
+OPENAI_MAX_TOKENS = 1000
+OPENAI_TOP_P = 1.0
 
-st.caption("Note that this demo app isn't actually connected to any LLMs. Those are expensive ;)")
+SYSTEM_PROMPT = ""
+
+openai_client = OpenAI(
+    base_url = OPENAI_URL, 
+    api_key =OPENAI_KEY,
+    timeout = OPENAI_TIMEOUT)
+
+st.set_page_config(page_title="Ask SBC Anything", page_icon=":smiley:", layout="wide")
+
+st.title("Ask Summary of Benefits and Coverage (SBC) Anything!")
+
+st.write("""To help consumers compare the different features of health benefits and coverage, the Affordable Care Act generally requires all group health plans and health insurance companies to provide individuals a “summary of benefits and coverage” that “accurately describes the benefits and coverage under the plan. The SBC is a snapshot of a health plan’s costs, benefits, covered health care services, and other features that are important to consumers. SBCs also explain health plans’ unique features like cost sharing rules and include significant limits and exceptions to coverage in easy-tounderstand terms.""")
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -17,6 +35,8 @@ for message in st.session_state.messages:
 
 # Accept user input
 if prompt := st.chat_input("What is up?"):
+    print (f"Prompt: {prompt}")
+
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
     # Display user message in chat message container
@@ -25,22 +45,21 @@ if prompt := st.chat_input("What is up?"):
 
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = ""
-        assistant_response = random.choice(
-            [
-                "Hello there! How can I assist you today?",
-                "Hi, human! Is there anything I can help you with?",
-                "Do you need help?",
-            ]
+        response_st = st.empty()
+    
+        print ("BEFORE")
+        # seek response from the llm
+        completion = openai_client.chat.completions.create(
+            model = OPENAI_MODEL,
+            messages = st.session_state.messages,
+            temperature = OPENAI_TEMPERATURE,
+            max_tokens = OPENAI_MAX_TOKENS,
+            top_p = OPENAI_TOP_P
         )
-        # Simulate stream of response with milliseconds delay
-        for chunk in assistant_response.split():
-            full_response += chunk + " "
-            time.sleep(0.05)
-            # Add a blinking cursor to simulate typing
-            message_placeholder.markdown(full_response + "▌")
-        message_placeholder.markdown(full_response)
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+        assistant_response = completion.choices[0].message.content
+        response_st.markdown(assistant_response)
+        print (f"RESPONSE: {assistant_response}")
 
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": assistant_response})
+    print (f"HISTORY: {st.session_state.messages}")
